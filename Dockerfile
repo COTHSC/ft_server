e4a3b8a78222bfec28009f0dcd06d04a4cd0dd80
jean <thierryscully.colomban@gmail.com>
2021-02-07 12:19:29 +0100

    added php my admin

--- Dockerfile
+++ Dockerfile
@@ -6,8 +6,9 @@ RUN apt-get install php php-fpm php-mysql -y
 RUN apt-get install nginx -y
 RUN apt-get install procps -y
 RUN apt-get install mariadb-server -y
+RUN apt-get install wget -y
 
 COPY mysite /etc/nginx/sites-available/
-COPY info.php /var/www/html/
+COPY info.php /var/www/mysite/
 COPY innit.sh /
 CMD bash innit.sh
--- innit.sh
+++ innit.sh
@@ -1,7 +1,12 @@
 mkdir /var/www/mysite
 chown -R /var/www/mysite
 chmod -R 765 /var/www/mysite
-sudo ln -s /etc/nginx/sites-available/mysite /etc/nginx/sites-enabled/
+ln -s /etc/nginx/sites-available/mysite /etc/nginx/sites-enabled/
+rm /etc/nginx/sites-enabled/default
 service nginx start
 service php7.3-fpm start
+service mysql start
+echo "CREATE DATABASE this_is_my_db;" | mysql -u root --skip-password
+echo "GRANT ALL ON this_is_my_db.* TO 'jescully'@'localhost' IDENTIFIED BY '1' WITH GRANT OPTION;" | mysql
+echo "FLUSH PRIVILEGES;" | mysql
 while true; do sleep 1000; done
